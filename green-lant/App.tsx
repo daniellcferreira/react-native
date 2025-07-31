b1934ba72b8b0aba65a220aa5d6c9e603359f501
Daniel Ferreira <dcfer2017@gmail.com>
2025-07-31 11:14:06 -0300

    feat: implementar toggle de imagem e background com useState

--- green-lant/App.tsx
+++ green-lant/App.tsx
@@ -1,19 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import symbolOn from "./assets/pictures/symbol-on.png"
+import symbolOff from "./assets/pictures/symbol-off.png"
 
 export default function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleSymbol() {
+    setIsActive((oldValue) => {
+      return !oldValue;
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Image source={symbolOn}></Image>  
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={() => {handleSymbol()}}>
+        <Image source={isActive ? symbolOn : symbolOff} />
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  containerOff: {
+    flex: 1,
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
