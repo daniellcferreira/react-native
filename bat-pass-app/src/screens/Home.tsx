c1f8c2d41fcd704b06c85ee2a808bed1df60498c
Daniel Ferreira <dcfer2017@gmail.com>
2025-08-04 10:24:30 -0300

    refactor: extrai estilos do componente Home para src/screens/Styles.tsx

--- bat-pass-app/src/screens/Home.tsx
+++ bat-pass-app/src/screens/Home.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text} from "react-native";
+import styles from "../screens/Styles"
 
 export default function Home() {
   return (
@@ -11,11 +12,3 @@ export default function Home() {
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
