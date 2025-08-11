022c868de940fa15a3ef98ac6447e0e1d80c09f9
Daniel Ferreira <dcfer2017@gmail.com>
2025-08-11 11:02:14 -0300

    feat(screens): adiciona telas Home e Gallery e integra ao App

--- react-nav/App.tsx
+++ react-nav/App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import GalleryScreen from './src/screens/GalleryScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <HomeScreen />
+      <GalleryScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ react-nav/src/screens/GalleryScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex:1, backgroundColor: 'blue'}}>
+      <Text>Gallery works!!!</Text>
+    </View>
+  )
+}
