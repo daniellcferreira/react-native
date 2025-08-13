d99fdc9de4a762a978461bf61625ed602aad2c30
Daniel Ferreira <dcfer2017@gmail.com>
2025-08-13 16:05:28 -0300

    feat: adicionar formulário no HomeScreen e navegação com parâmetro para UserScreen

--- context-example/src/screens/HomeScreen.tsx
+++ context-example/src/screens/HomeScreen.tsx
@@ -1,9 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
+import { useState } from "react";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../../App";
 
-export default function HomeScreen() {
+type HomeScreenProps = {
+  navigation: StackNavigationProp<RootStackParamList, 'Home'>;
+}
+
+export default function HomeScreen({ navigation }: HomeScreenProps) {
+  const [inputText, setInputText] = useState('');
+
+  const navigateToUserScreen = () => {
+    navigation.navigate('User', { userName: inputText });
+  }
+
   return(
     <View>
-      <Text>Home Screen</Text>
+      <TextInput
+        placeholder="Digite seu nome: " 
+        value={inputText}
+        onChangeText={(text) => setInputText(text)}/>
+      <Button title="Logar" onPress={navigateToUserScreen}/>
     </View>
   ) 
 }
