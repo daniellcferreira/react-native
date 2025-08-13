2da4b5341cdd7a24bbc092f8dbca437d967fc86a
Daniel Ferreira <dcfer2017@gmail.com>
2025-08-13 11:04:59 -0300

    feat: adiciona controle de quantidade e cálculo de preço no App

--- my-hooks/App.tsx
+++ my-hooks/App.tsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [quantity, setQuantity] = useState<number>(1);
+  const [price, setPrice] = useState<number>(10.9);
+  const initialPrice = 10.9;
+
+  const increment = () => {
+    setQuantity((prevQuantity) => {
+      const newQuantity = prevQuantity + 1;
+      setPrice(initialPrice * newQuantity);
+      return newQuantity;
+    });
+  };
+
+  const decrement = () => {
+    setQuantity((prevQuantity) => {
+      const newQuantity = prevQuantity - 1;
+      setPrice(initialPrice * newQuantity);
+      return newQuantity;
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.buttonRow}>
+        <Button title='-' onPress={decrement}/>   
+        <Text style={styles.textLabel}>{quantity}</Text>
+        <Button title='+' onPress={increment}/>
+      </View>
+      <Text style={styles.textLabel}>Price: ${price.toFixed(2)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +43,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textLabel: {
+    fontSize: 22,
+    color: 'blue',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    padding: 15,
+  }
 });
