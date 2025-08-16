e605136f5b7b35f5631be92758fcd8025207c3c9
Daniel Ferreira <dcfer2017@gmail.com>
2025-08-16 11:51:41 -0300

    feat: criar integração com API de carros, adicionar actions e atualizar CardView para navegação dinâmica

--- lamborghini-garage/src/components/CardView/index.tsx
+++ lamborghini-garage/src/components/CardView/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import { View, Text, Button, Image } from 'react-native';
 import { styles } from './styles';
@@ -5,8 +6,18 @@ import Logo from '../../../assets/logo.png';
 import Divider from '../Divider';
 import { CAR_ASSETS_BASE_URL } from '../../constants/car';
 import BuyButton from '../BuyButton';
+import { CarModel } from './props';
+import { handleNextItem, handlePreviousItem, loadCarData } from './actions';
 
 export default function CardView() {
+
+  const [carData, setCarData] = useState<CarModel | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      await loadCarData(3, setCarData);
+    })();
+  }, []);
 
   const renderLogoBox = () => {
     return (
@@ -20,7 +31,7 @@ export default function CardView() {
     return (
       <View style={{ alignItems: 'center'}}>
         <Text style={styles.carBrand}>Lamborghini</Text>
-        <Text style={styles.carName}>MODEL</Text>
+        <Text style={styles.carName}>{carData?.carName}</Text>
       </View>
       
     )
@@ -30,7 +41,7 @@ export default function CardView() {
     return (
       <Image 
         style={styles.image}
-        source={{uri: `${CAR_ASSETS_BASE_URL}2.png`}}
+        source={{uri: `${CAR_ASSETS_BASE_URL}${carData?.id}.png`}}
       />
     )
   }
@@ -38,9 +49,9 @@ export default function CardView() {
   const renderPriceControls = () => {
     return (
       <View style={styles.priceContainer}>
-        <Button title='<' color={'#01A6B3'} onPress={() => {}}/>
-        <Text style={styles.priceText}>VALOR</Text>
-        <Button title='>'color={'#01A6B3'} onPress={() => {}}/>
+        <Button title='<' color={'#01A6B3'} onPress={() => handlePreviousItem(carData, setCarData)}/>
+        <Text style={styles.priceText}>{carData?.price}</Text>
+        <Button title='>'color={'#01A6B3'} onPress={() => handleNextItem(carData, setCarData)}/>
       </View>
     )
   }
